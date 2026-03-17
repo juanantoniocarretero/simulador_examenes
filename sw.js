d3719cda7ffea1d69b48f41b7fd193723f9a3200
juanantoniocarretero <j.antonio.carretero@gmail.com>
2026-03-17 13:37:10 +0100

    Configuramos para uso Offline tambien

--- sw.js
+++ sw.js
@@ -1,5 +1,28 @@
-// sw.js - Service Worker básico
+const CACHE_NAME = 'simulador-certs-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js',
+  './js/storage.js',
+  './js/uiManager.js',
+  './manifest.json'
+];
+
+// Instalación: Guarda los archivos en la memoria del móvil
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Estrategia: Intentar red, si falla, usar lo guardado en caché
 self.addEventListener('fetch', (event) => {
-  // Este código permite que la app funcione incluso con red inestable
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
